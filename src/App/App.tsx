0d31b3a296b731449bd8f6379fd2d01d5f573d56
IannRedzio <ianredzio13@gmail.com>
2025-07-12 23:42:21 -0300

    Initial Commit

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,13 +2,12 @@ import React from "react";
 
 import logo from "../assets/logo.png";
 
-import styles from "./App.module.scss";
 
 function App() {
   return (
-    <main>
+    <main className="bg-sky-500 min-h-screen w-full flex flex-col items-center justify-center">
       <img alt="Softvision" src={logo} width={320} />
-      <h1 className={styles.title}>Lets get this party started</h1>
+      <h1>Lets get this party started</h1>
     </main>
   );
 }
